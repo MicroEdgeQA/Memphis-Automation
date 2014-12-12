e5124dabc07a408b9ef08bdef72ee84e1aad493b
Luis bello <lbello@microedge.com>
2014-12-12 16:05:10 -0500

    updated from Michael's code

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -4,10 +4,12 @@ import common.Util;
 
 import java.util.Properties;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Login
@@ -16,50 +18,82 @@ public class Login
 	public WebDriver driver = new FirefoxDriver();
 	private Properties prop;
 	
-	@BeforeClass
-	public void launchBrowser()
+	public void launchSite()
 	{
+		driver.get("about:blank");
 		driver.get(baseURL);
-		prop = Util.getPageProperties();
+	}
+		
+	@AfterClass
+	public void closeBrowser()
+	{
+		driver.quit();
+	}
+	
+	@BeforeMethod
+	public void checkPageLoaded()
+	{
+		Assert.assertEquals(Util.IsDOMReady(driver), true);
 	}
 	
 	@Test(description="Successful Login")
 	public void loginSuccessful()
-	{
-		System.out.println("Successful Login");
+	{	
+		launchSite();
 		
+		prop = Util.getPageProperties("LoginPage");
+		
+		String userIDExpected = "admin";
+		String passwordExpected = "microedge";
+		
+		String defaultUserIDTextExpected = "User ID";
+		String defaultPasswordTextExpected = "Password";
+		
+		// Check placeholder text in User ID field
+		String defaultUserIDTextActual = driver.findElement(By.id(prop.getProperty("userIDField"))).getAttribute("placeholder");
+		Assert.assertEquals(defaultUserIDTextExpected, defaultUserIDTextActual);
+		
+		// Check placeholder text in Password field
+		String defaultPasswordTextActual = driver.findElement(By.id(prop.getProperty("passwordField"))).getAttribute("placeholder");
+		Assert.assertEquals(defaultPasswordTextExpected, defaultPasswordTextActual);
+
+		// Enter User ID and check text appears in User ID field
 		driver.findElement(By.id(prop.getProperty("userIDField"))).sendKeys("admin");
+		String userIDActual = driver.findElement(By.id(prop.getProperty("userIDField"))).getAttribute("value");
+		Assert.assertEquals(userIDExpected, userIDActual);
+				
+		// Enter Password and check text appears in Password field
 		driver.findElement(By.id(prop.getProperty("passwordField"))).sendKeys("microedge");
+		String passwordActual = driver.findElement(By.id(prop.getProperty("passwordField"))).getAttribute("value");
+		Assert.assertEquals(passwordExpected, passwordActual);
+		
+		// Click Login button
 		driver.findElement(By.className(prop.getProperty("loginButton"))).click();
 	}
 	
 	@Test(description="Missing User ID")
 	public void missingUserID()
-	{
-		System.out.println("Missing User ID");
+	{	
+		launchSite();
+		
+		String userIDRequiredExpected = "User ID is Required.";
+		
+		driver.findElement(By.className(prop.getProperty("loginButton"))).click();
+		String userIDRequiredActual = driver.findElement(By.className(prop.getProperty("userIDRequired"))).getText();
+		Assert.assertEquals(userIDRequiredExpected, userIDRequiredActual);
 	}
 	
 	@Test(description="Incorrect User ID or Password")
 	public void incorrectUserIDPassword()
-	{
-		System.out.println("Incorrect User ID or Password");
+	{	
+		launchSite();
+		
+		String incorrectUserIDPasswordExpected = "Incorrect user ID or password.";
+		
+		driver.findElement(By.id(prop.getProperty("userIDField"))).sendKeys("admin");
+		driver.findElement(By.className(prop.getProperty("loginButton"))).click();
+		
+		String incorrectUserIDPasswordActual = driver.findElement(By.xpath(prop.getProperty("incorrectUserIDPassword"))).getText();
+		Assert.assertEquals(incorrectUserIDPasswordExpected, incorrectUserIDPasswordActual);
 	}
-	
-	@Test(description="Forgot Password")
-	public void forgotPassword()
-	{
-		System.out.println("Forgot Password");
-	}
-	
-	@Test(description="Header")
-	public void header()
-	{
-		System.out.println("Header");
-	}	
-	
-	@Test(description="Footer")
-	public void footer()
-	{
-		System.out.println("Footer");
-	}	
 }
